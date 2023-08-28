22233dc6c32061eceef41570e66347c5d42a71dd
Matthew Smith Warren <matthewsmithwarren@gmail.com>
2023-08-28 15:57:39 -0400

    Update app.py
    Added guidance to register in the Login error message.

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username and/or password. You may need to Register. ", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
